e531da451688e2198caf520066453ddda007e7f0
Giovanny Cacua Pedraza <giovannycacua3@gmail.com>
2025-07-22 17:32:50 +0000

    eliminamos linea en blanco

--- src/api/models.py
+++ src/api/models.py
@@ -38,7 +38,6 @@ class Users(db.Model):
                 "role": self.role,}
                 
 
-
 class Trips(db.Model):
     __tablename__ = 'trips'
     id = db.Column(db.Integer, primary_key=True)
